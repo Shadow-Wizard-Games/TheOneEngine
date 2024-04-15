fcfbb5572e6a534b4481cbcbb478789a16107bc0
faelion <alexdlinde@gmail.com>
2024-04-16 01:27:14 +0200

    New tools & functions to import .fbx to .ozzmesh

--- TheOneEngine/Model.cpp
+++ TheOneEngine/Model.cpp
@@ -1,5 +1,5 @@
 #include "Model.h"
-#include "Log.h"
+#include "Defs.h"
 
 #include <assimp/postprocess.h>
 #include <assimp/cimport.h>
@@ -8,7 +8,31 @@
 #include <fstream>
 #include "Resources.h"
 
-Model::Model(const std::string& filename)
+#include "ozz/animation/runtime/animation.h"
+#include "ozz/animation/runtime/blending_job.h"
+#include "ozz/animation/runtime/local_to_model_job.h"
+#include "ozz/animation/runtime/sampling_job.h"
+#include "ozz/animation/runtime/skeleton.h"
+#include "ozz/animation/runtime/skeleton_utils.h"
+#include "ozz/base/containers/vector.h"
+#include "ozz/base/log.h"
+#include "ozz/base/maths/simd_math.h"
+#include "ozz/base/maths/soa_transform.h"
+#include "ozz/base/maths/vec_float.h"
+#include "ozz/options/options.h"
+
+#include "Animation/animations/OzzAnimationSimple.h"
+
+Model::Model(const std::string& filename) :
+m_ActiveAnimationId(INVALID_INDEX),
+m_PrevAnimationId(INVALID_INDEX),
+m_LoadedMesh(false),
+m_LoadedSkeleton(false),
+m_BlendOnTransition(true),
+m_BlendThreshold(ozz::animation::BlendingJob().threshold),
+m_TransitionTime(1.0f),
+m_TransitionTimer(0.0f),
+m_UpdateState(US_UPDATE)
 {
     // This constructor only loads .mesh
     if (filename.ends_with(".mesh"))
@@ -46,6 +70,15 @@ std::vector<Model*> Model::LoadMeshes(const std::string& path)
         vec3f* verts = (vec3f*)mesh->mVertices;
         vec3f* texCoords = (vec3f*)mesh->mTextureCoords[0];
 
+        // Process Anim
+        if (mesh->HasBones())
+        {
+            // TODO: Cosa de importar el fbx a .mesh del ozz con el fbx2mesh.exe
+            //std::string importPath = Resources::PathToLibrary<Model>(sceneName) + model->meshName;
+            //model->ImportToOzz(path, importPath);
+
+            //TODO: Process new materials for animated meshes
+        }
 
         // Process materials
         if (scene->HasMaterials())
@@ -424,4 +457,413 @@ void Model::Render()
     meshVAO->Bind();
     GLCALL(glDrawElements(GL_TRIANGLES, (GLsizei)indexData.size(), GL_UNSIGNED_INT, 0));
     meshVAO->Unbind();
+}
+
+/*======================================================= OZZ ANIMATIOR MESH =======================================================*/
+
+bool Model::LoadSkeleton(const std::string& path)
+{
+    m_LoadedSkeleton = ozz::sample::LoadSkeleton(path.c_str(), &m_Skeleton);
+
+    if (m_LoadedSkeleton)
+    {
+        m_SkeletonPath = path;
+
+        // Allocates runtime buffers.
+        const int num_joints = m_Skeleton.num_joints();
+        const int num_soa_joints = m_Skeleton.num_soa_joints();
+
+        models_.resize(num_joints);
+        // Allocates local space runtime buffers of blended data.
+        blended_locals_.resize(num_soa_joints);
+    }
+
+    return m_LoadedSkeleton;
+}
+
+
+size_t Model::_create_anim_impl()
+{
+    size_t index = 0;
+
+    if (m_RemovedAnimationsIndex.size() > 0)
+    {
+        index = m_RemovedAnimationsIndex[m_RemovedAnimationsIndex.size() - 1];
+        m_RemovedAnimationsIndex.pop_back();
+    }
+    else
+    {
+        index = m_AnimationList.size();
+        m_AnimationList.emplace_back();
+    }
+
+    return index;
+}
+
+bool Model::_update(float _dt)
+{
+    AnimationData& a_data = m_AnimationList[m_ActiveAnimationId];
+
+    OzzAnimation* animation = a_data.animation;
+
+    if (!animation->Update(_dt)) return false;
+
+    ozz::vector<ozz::math::SoaTransform>& locals = animation->getLocals();
+
+    // Converts from local space to model space matrices.
+    // Gets the output of the blending stage, and converts it to model space.
+
+    // Setup local-to-model conversion job.
+    ozz::animation::LocalToModelJob ltm_job;
+    ltm_job.skeleton = &m_Skeleton;
+    ltm_job.input = make_span(locals);
+    ltm_job.output = make_span(models_);
+
+    // Run ltm job.
+    if (!ltm_job.Run())
+        return false;
+
+    // Build skinning matrices
+    for (size_t i = 0; i < m_Mesh.joint_remaps.size(); ++i)
+        skinning_matrices_[i] = models_[m_Mesh.joint_remaps[i]] * m_Mesh.inverse_bind_poses[i];
+
+    return true;
+}
+
+bool Model::_update_blend(float _dt)
+{
+    float weight_ratio = m_TransitionTimer / m_TransitionTime;
+
+    AnimationData& a_data = m_AnimationList[m_ActiveAnimationId];
+    AnimationData& pa_data = m_AnimationList[m_PrevAnimationId];
+
+    OzzAnimation* animation = a_data.animation;
+    OzzAnimation* prevanimation = pa_data.animation;
+
+    if (!animation->Update(_dt)) return false;
+    if (!prevanimation->Update(_dt)) return false;
+
+    ozz::vector<ozz::math::SoaTransform>& locals = animation->getLocals();
+    ozz::vector<ozz::math::SoaTransform>& prevlocals = prevanimation->getLocals();
+
+    // Prepares blending layers.
+    ozz::animation::BlendingJob::Layer layers[2];
+    layers[0].transform = make_span(locals);
+    layers[0].weight = weight_ratio;
+
+    layers[1].transform = make_span(prevlocals);
+    layers[1].weight = 1.0f - weight_ratio;
+
+    // Setups blending job.
+    ozz::animation::BlendingJob blend_job;
+    blend_job.threshold = m_BlendThreshold;
+    blend_job.layers = layers;
+    blend_job.rest_pose = m_Skeleton.joint_rest_poses();
+    blend_job.output = make_span(blended_locals_);
+
+    // Blends.
+    if (!blend_job.Run())
+        return false;
+
+    // Setup local-to-model conversion job.
+    ozz::animation::LocalToModelJob ltm_job;
+    ltm_job.skeleton = &m_Skeleton;
+    ltm_job.input = make_span(blended_locals_);
+    ltm_job.output = make_span(models_);
+
+    // Run ltm job.
+    if (!ltm_job.Run())
+        return false;
+
+    // Build skinning matrices
+    for (size_t i = 0; i < m_Mesh.joint_remaps.size(); ++i)
+        skinning_matrices_[i] = models_[m_Mesh.joint_remaps[i]] * m_Mesh.inverse_bind_poses[i];
+
+    m_TransitionTimer += _dt;
+
+    if (m_TransitionTimer >= m_TransitionTime)
+    {
+        m_UpdateState = US_UPDATE;
+        m_TransitionTimer = 0.0f;
+    }
+
+    return true;
+}
+
+ozz::span<const char* const> Model::getSkeletonBoneNames()
+{
+    if (!m_LoadedSkeleton)
+        return ozz::span<const char* const>();
+
+    return m_Skeleton.joint_names();
+}
+
+size_t Model::CreateSimpleAnimation(const std::string& name)
+{
+    // Return NULL if name already in list
+    if (HasAnimation(name)) return INVALID_INDEX;
+
+    size_t anim_id = _create_anim_impl();
+
+    // Add index to map
+    m_AnimationsIndex[name] = anim_id;
+
+    // Create partial animation
+    OzzAnimationSimple* simple_anim = new OzzAnimationSimple();
+    simple_anim->SetSkeleton(&m_Skeleton);
+
+    AnimationData& a_data = m_AnimationList[anim_id];
+
+    a_data.name = name;
+    a_data.animation = simple_anim;
+
+    return anim_id;
+}
+
+
+size_t Model::CreatePartialAnimation(const std::string& name)
+{
+    // Return NULL if name already in list
+    if (HasAnimation(name)) return INVALID_INDEX;
+
+    size_t anim_id = _create_anim_impl();
+
+    // Add index to map
+    m_AnimationsIndex[name] = anim_id;
+
+    // Create partial animation
+    OzzAnimationPartialBlending* partial_anim = new OzzAnimationPartialBlending();
+    partial_anim->SetSkeleton(&m_Skeleton);
+
+    AnimationData& a_data = m_AnimationList[anim_id];
+
+    a_data.name = name;
+    a_data.animation = partial_anim;
+
+    return anim_id;
+}
+
+void Model::RemoveAnimationAt(size_t index)
+{
+    AnimationData& a_data = m_AnimationList[index];
+
+    m_RemovedAnimationsIndex.push_back(index);
+    m_AnimationsIndex.erase(a_data.name);
+
+    a_data.name = "Removed";
+    delete a_data.animation;
+    a_data.animation = nullptr;
+
+    if (m_ActiveAnimationId == index)
+    {
+        m_ActiveAnimationId = INVALID_INDEX;
+        m_ActiveAnimationName = "";
+    }
+}
+
+bool Model::HasAnimation(const std::string& name)
+{
+    return m_AnimationsIndex.find(name) != m_AnimationsIndex.end();
+}
+
+Model::AnimationData* Model::PlayAnimation(const std::string& name, float time_ratio)
+{
+    std::unordered_map<std::string, size_t>::iterator it = m_AnimationsIndex.find(name);
+
+    if (it != m_AnimationsIndex.end())
+        return PlayAnimation(it->second, time_ratio);
+
+    return nullptr;
+}
+
+Model::AnimationData* Model::PlayAnimation(size_t anim_id, float time_ratio)
+{
+    if (anim_id >= 0 && anim_id < m_AnimationList.size())
+    {
+        AnimationData& a_data = m_AnimationList[anim_id];
+        a_data.animation->Play();
+
+        if (anim_id == m_ActiveAnimationId)
+        {
+            if (!a_data.animation->HasFinished() || a_data.animation->getLoop())
+                return &a_data;
+        }
+
+        a_data.animation->SetTimeRatio(time_ratio);
+
+        if (a_data.animation->getStatus() == OzzAnimation::Status::VALID)
+        {
+            if (m_ActiveAnimationId != INVALID_INDEX)
+            {
+                if (m_BlendOnTransition)
+                {
+                    m_UpdateState = US_BLEND;
+                    m_PrevAnimationId = m_ActiveAnimationId;
+                }
+            }
+
+            m_ActiveAnimationId = anim_id;
+            m_ActiveAnimationName = a_data.name;
+
+            return &m_AnimationList[m_ActiveAnimationId];
+        }
+
+        return nullptr;
+    }
+
+    return nullptr;
+}
+
+void Model::StopAnimation(bool unbind)
+{
+    if (m_ActiveAnimationId != INVALID_INDEX)
+    {
+        AnimationData& a_data = m_AnimationList[m_ActiveAnimationId];
+
+        a_data.animation->Stop();
+
+        if (unbind)
+        {
+            m_ActiveAnimationId = INVALID_INDEX;
+            m_ActiveAnimationName = "";
+        }
+    }
+}
+
+OzzAnimation* Model::getAnimationByName(const std::string& name)
+{
+    std::unordered_map<std::string, size_t>::iterator it = m_AnimationsIndex.find(name);
+
+    if (it != m_AnimationsIndex.end())
+        return m_AnimationList[it->second].animation;
+
+    return nullptr;
+}
+
+size_t Model::getAnimationIndex(const std::string& str)
+{
+    std::unordered_map<std::string, size_t>::iterator it = m_AnimationsIndex.find(str);
+
+    if (it != m_AnimationsIndex.end())
+        return it->second;
+
+    return INVALID_INDEX;
+}
+
+OzzAnimation* Model::getActiveAnimation()
+{
+    OzzAnimation* anim = nullptr;
+
+    if (m_ActiveAnimationId != INVALID_INDEX)
+        anim = m_AnimationList[m_ActiveAnimationId].animation;
+
+    return anim;
+}
+
+void Model::SetUpperPlaybackSpeed(size_t anim_id, float playback_speed)
+{
+    if (anim_id < 0 || anim_id >= m_AnimationList.size()) return;
+
+    AnimationData& a_data = m_AnimationList[anim_id];
+
+    if (!a_data.animation) return;
+
+    if (a_data.animation->getAnimationType() == AnimationType::AT_PARTIAL_BLEND)
+    {
+        OzzAnimationPartialBlending* a_partial = (OzzAnimationPartialBlending*)a_data.animation;
+        a_partial->SetUpperPlaybackSpeed(playback_speed);
+    }
+}
+
+void Model::SetLowerPlaybackSpeed(size_t anim_id, float playback_speed)
+{
+    if (anim_id < 0 || anim_id >= m_AnimationList.size()) return;
+
+    AnimationData& a_data = m_AnimationList[anim_id];
+
+    if (!a_data.animation) return;
+
+    if (a_data.animation->getAnimationType() == AnimationType::AT_PARTIAL_BLEND)
+    {
+        OzzAnimationPartialBlending* a_partial = (OzzAnimationPartialBlending*)a_data.animation;
+        a_partial->SetLowerPlaybackSpeed(playback_speed);
+    }
+}
+
+bool Model::UpdateAnim(float _dt)
+{
+    if (m_ActiveAnimationId != INVALID_INDEX)
+    {
+        switch (m_UpdateState)
+        {
+        case US_UPDATE: _update(_dt); break;
+        case US_BLEND: _update_blend(_dt); break;
+        default: break;
+        }
+    }
+
+    return true;
+}
+
+void Model::ImportToOzz(const std::string& file, const std::filesystem::path& importPath)
+{
+    std::string filename = importPath.filename().string();
+
+    // Skeleton output path
+    std::filesystem::path skeleton_path = importPath;
+    skeleton_path.replace_filename(filename + "_skeleton");
+    skeleton_path.replace_extension(".skeleton");
+
+    // Animations output path
+    std::filesystem::path anim_out_path = importPath;
+    anim_out_path.replace_filename(filename + "_*");
+    anim_out_path.replace_extension(".anim");
+
+
+    // Mesh output path
+    std::filesystem::path mesh_out_path = importPath;
+    mesh_out_path.replace_filename(filename + "_animatedmesh");
+    mesh_out_path.replace_extension(".mesh");
+    path = mesh_out_path.string();
+
+    // === Setup ozz import json config ===
+    json ozzImportJSON;
+
+    // skeleton settings
+    json skeletonJSON;
+
+    // skeleton filename
+    skeletonJSON["filename"] = skeleton_path.string().c_str();
+
+    // skeleton import settings
+    json skeletonImportJSON;
+    skeletonImportJSON["enable"] = true;
+    skeletonImportJSON["raw"] = false;
+
+    skeletonJSON["import"] = skeletonImportJSON;
+
+    ozzImportJSON["skeleton"] = skeletonJSON;
+
+    // animation settings
+    json animationsJSON;
+    json animJSON;
+    animJSON["clip"] = "*";
+    animJSON["filename"] = anim_out_path.string().c_str();
+
+    animationsJSON = animJSON;
+
+    ozzImportJSON = animationsJSON;
+
+    Resources::SaveJSON("Tools\\import_anim.json", ozzImportJSON);
+
+    // === Import ozz ===
+    std::string cmd = "Tools\\import_anim.bat";
+    cmd += " \"";
+    cmd += file.c_str(); // Path of file to import (.fbx)
+    cmd += "\" \"";
+    cmd += mesh_out_path.string().c_str(); // Path of output mesh
+    cmd += "\" \"";
+    cmd += skeleton_path.string().c_str(); // Path of skeleton to import mesh
+    cmd += "\"";
+    system(cmd.c_str());
 }
--- TheOneEngine/Model.h
+++ TheOneEngine/Model.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "VertexArray.h"
+#include "Animation/animations/OzzAnimationPartialBlending.h"
 
 
 class Texture;
@@ -16,6 +17,12 @@ class Model
 public:
     friend class ModelScene;
 
+    struct AnimationData
+    {
+        OzzAnimation* animation;
+        std::string name;
+    };
+
     Model(){}
     Model(const std::string& path);
     ~Model(){}
@@ -26,6 +33,7 @@ public:
     void Render();
 
     std::string GetMaterialPath() { return materials[materialIndex]; }
+    std::string GetMeshPath() { return path; }
 
     std::string meshName;
     std::vector<float> vertexData;
@@ -54,4 +62,123 @@ private:
 
     std::vector<std::string> materials;
     uint materialIndex = 0;
+
+
+    /*===================================== OZZ MESH =====================================*/
+    enum UpdateState
+    {
+        US_BLEND,
+        US_UPDATE,
+        US_LAST
+    };
+
+    UpdateState m_UpdateState;
+
+    bool m_BlendOnTransition;
+    float m_BlendThreshold;
+    float m_TransitionTime;
+    float m_TransitionTimer;
+
+    ozz::sample::Mesh m_Mesh;
+    bool m_LoadedMesh;
+
+    std::string m_SkeletonPath;
+    ozz::animation::Skeleton m_Skeleton;
+    bool m_LoadedSkeleton;
+
+    std::string m_ActiveAnimationName;
+    size_t m_ActiveAnimationId;
+    size_t m_PrevAnimationId;
+
+    // Buffer of model space matrices. These are computed by the local-to-model
+    // job after the blending stage.
+    ozz::vector<ozz::math::Float4x4> models_;
+
+    // Buffer of skinning matrices, result of the joint multiplication of the
+    // inverse bind pose with the model space matrix.
+    ozz::vector<ozz::math::Float4x4> skinning_matrices_;
+
+    // Buffer of local transforms which stores the blending result.
+    ozz::vector<ozz::math::SoaTransform> blended_locals_;
+
+    std::vector<AnimationData> m_AnimationList;
+    std::unordered_map<std::string, size_t> m_AnimationsIndex;
+
+    std::vector<size_t> m_RemovedAnimationsIndex;
+
+    size_t _create_anim_impl();
+    bool _update(float _dt);
+    bool _update_blend(float _dt);
+    void ImportToOzz(const std::string& file, const std::filesystem::path& importPath);
+public:
+
+	bool IsMeshLoaded() { return m_LoadedMesh; }
+
+	bool LoadSkeleton(const std::string& path);
+	bool IsSkeletonLoaded() { return m_LoadedSkeleton; }
+	std::string getSkeletonPath() { return m_SkeletonPath; }
+
+	// Get skeleton bone names
+	ozz::span<const char* const> getSkeletonBoneNames();
+
+	// Create simple animation
+	size_t CreateSimpleAnimation(const std::string& name);
+
+	// Create partial animation
+	size_t CreatePartialAnimation(const std::string& name);
+
+	// Remove animation at index
+	void RemoveAnimationAt(size_t index);
+
+	// Check if animation name exists
+	bool HasAnimation(const std::string& name);
+
+	// Play animation by name
+	AnimationData* PlayAnimation(const std::string& name, float time_ratio = 0.0f);
+
+	// Play animation by id
+	AnimationData* PlayAnimation(size_t anim_id, float time_ratio = 0.0f);
+
+	// Stop animation
+	void StopAnimation(bool unbind = true);
+
+
+    // Set transition blend bool
+    void setBlendOnTransition(bool blend) { m_BlendOnTransition = blend; }
+
+    // Get transition blend bool
+    bool getBlendOnTransition() { return m_BlendOnTransition; }
+
+    // Set transition time for blending
+    void setTransitionTime(float time) { m_TransitionTime = time; };
+
+    // Get transition time for blending
+    float getTransitionTime() { return m_TransitionTime; }
+	// Get total animation count (includes removed animations)
+	size_t getAnimationCount() { return m_AnimationList.size(); }
+	AnimationData& getAnimationAt(size_t index) { return m_AnimationList[index]; }
+	OzzAnimation* getAnimationByName(const std::string& name);
+	size_t getAnimationIndex(const std::string& str);
+	size_t getActiveAnimationId() { return m_ActiveAnimationId; }
+	OzzAnimation* getActiveAnimation();
+	std::string getActiveAnimationName() { return m_ActiveAnimationName; }
+
+	// Set upper animation playback speed
+	void SetUpperPlaybackSpeed(size_t anim_id, float playback_speed);
+
+	// Set lower animation playback speed
+	void SetLowerPlaybackSpeed(size_t anim_id, float playback_speed);
+
+	// Update
+	bool UpdateAnim(float _dt);
+
+	// Render
+	//bool Render(Camera* camera, mat4 transform);
+
+    // TODO =>
+	// Static save function
+	//void SaveAnimator(const char* filepath);
+
+	// Static load function
+	//void LoadAnimator(const char* filepath);
 };
