4c94c17c94d1ecb07fdda567381bb0ffd54d98cc
Héctor Báscones <34044086+Hekbas@users.noreply.github.com>
2024-05-16 12:14:37 +0200

    Separate banks for audio

---
+++ TheOneEditor/Assets/Audio/Wwise Project/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,240 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID A_AR = 3814696296U;
+        static const AkUniqueID A_BANDAGE = 2650455497U;
+        static const AkUniqueID A_FT = 3730808143U;
+        static const AkUniqueID A_GL_EXPLOSION = 337852012U;
+        static const AkUniqueID A_GL_SHOOT = 323248236U;
+        static const AkUniqueID A_LI = 3630142368U;
+        static const AkUniqueID A_S_ACTIVATE = 1589784596U;
+        static const AkUniqueID A_S_DEACTIVATE = 1176085861U;
+        static const AkUniqueID A_S_HIT = 2965692658U;
+        static const AkUniqueID DEBUG_GUNSHOT = 1535709413U;
+        static const AkUniqueID DEBUG_MUSIC1 = 830312575U;
+        static const AkUniqueID DEBUG_MUSIC2 = 830312572U;
+        static const AkUniqueID DEBUG_SPATIAL1 = 3632435752U;
+        static const AkUniqueID DEBUG_SPATIAL2 = 3632435755U;
+        static const AkUniqueID E_A_DEATH = 1015701557U;
+        static const AkUniqueID E_A_SHOOT = 2923735218U;
+        static const AkUniqueID E_A_STEP = 1828976739U;
+        static const AkUniqueID E_CB_DEATH = 2100764501U;
+        static const AkUniqueID E_EGG_CRACK = 3286329979U;
+        static const AkUniqueID E_FH_CRAWL = 3938483819U;
+        static const AkUniqueID E_FH_DEATH = 4203716004U;
+        static const AkUniqueID E_FH_JUMP = 2711568226U;
+        static const AkUniqueID E_Q_BOMB = 692574897U;
+        static const AkUniqueID E_Q_CHANGE = 3169029109U;
+        static const AkUniqueID E_Q_DEATH = 610103877U;
+        static const AkUniqueID E_Q_JUMP = 3869385341U;
+        static const AkUniqueID E_Q_SPIT = 2254856839U;
+        static const AkUniqueID E_Q_SPITCHARGE = 3547019691U;
+        static const AkUniqueID E_Q_STEP = 2725321747U;
+        static const AkUniqueID E_Q_TAILSHOT = 2084671035U;
+        static const AkUniqueID E_Q_TAILSWEEP = 1606613397U;
+        static const AkUniqueID E_X_ADULT_DEATH = 3828971157U;
+        static const AkUniqueID E_X_ADULT_MELEE = 3271687631U;
+        static const AkUniqueID E_X_ADULT_SPAWN = 3991779178U;
+        static const AkUniqueID E_X_ADULT_SPIT = 2247807735U;
+        static const AkUniqueID E_X_ADULT_STEP = 1644504899U;
+        static const AkUniqueID P_DASH = 3919743498U;
+        static const AkUniqueID P_DEATH = 947529532U;
+        static const AkUniqueID P_HIT = 1073763149U;
+        static const AkUniqueID P_PICK = 1941667005U;
+        static const AkUniqueID P_ROLL = 2206507859U;
+        static const AkUniqueID P_STEP = 3875642140U;
+        static const AkUniqueID PLAYMUSIC = 417627684U;
+        static const AkUniqueID STOPMUSIC = 1917263390U;
+        static const AkUniqueID UI_A_MENU = 464895987U;
+        static const AkUniqueID UI_CLICK = 2249769530U;
+        static const AkUniqueID UI_HOVER = 2118900976U;
+        static const AkUniqueID UI_PAUSEGAME = 3783962382U;
+        static const AkUniqueID VO_COMPUTER_LOG1 = 1406454956U;
+        static const AkUniqueID VO_COMPUTER_LOG2 = 1406454959U;
+        static const AkUniqueID VO_COMPUTER_LOG3 = 1406454958U;
+        static const AkUniqueID VO_COMPUTER_LOG4 = 1406454953U;
+        static const AkUniqueID VO_COMPUTER_LOG5 = 1406454952U;
+        static const AkUniqueID VO_GRANT_BACKCAMP1 = 1236238417U;
+        static const AkUniqueID VO_GRANT_ENDMISSIONSTUB1 = 2538900238U;
+        static const AkUniqueID VO_GRANT_ENDMISSIONSTUB2 = 2538900237U;
+        static const AkUniqueID VO_GRANT_FOURTHCOMPUTER1 = 2056092986U;
+        static const AkUniqueID VO_GRANT_FOURTHCOMPUTER2 = 2056092985U;
+        static const AkUniqueID VO_GRANT_NOAH1 = 1121060561U;
+        static const AkUniqueID VO_GRANT_NOAH2 = 1121060562U;
+        static const AkUniqueID VO_GRANT_NOAH3 = 1121060563U;
+        static const AkUniqueID VO_GRANT_NOAH4 = 1121060564U;
+        static const AkUniqueID VO_GRANT_OUTSIDESTUB1 = 4061054028U;
+        static const AkUniqueID VO_GUARD1_VOICELINE1_V1 = 3357592761U;
+        static const AkUniqueID VO_GUARD1_VOICELINE1_V2 = 3357592762U;
+        static const AkUniqueID VO_GUARD1_VOICELINE2 = 234843012U;
+        static const AkUniqueID VO_GUARD2_VOICELINE1 = 4164671226U;
+        static const AkUniqueID VO_GUARD2_VOICELINE2 = 4164671225U;
+        static const AkUniqueID VO_ISAAC_FULLHEALTH = 3566892078U;
+        static const AkUniqueID VO_ISAAC_HEALTH1 = 3701460526U;
+        static const AkUniqueID VO_ISAAC_HEALTH2 = 3701460525U;
+        static const AkUniqueID VO_ISAAC_HEALTH3 = 3701460524U;
+        static const AkUniqueID VO_ISAAC_HEALTH4 = 3701460523U;
+        static const AkUniqueID VO_ISAAC_VOICELINE1 = 2021844418U;
+        static const AkUniqueID VO_ISAAC_VOICELINE2 = 2021844417U;
+        static const AkUniqueID VO_ISAAC_VOICELINE3 = 2021844416U;
+        static const AkUniqueID VO_ISAAC_VOICELINE4 = 2021844423U;
+        static const AkUniqueID VO_ISAAC_VOICELINE5 = 2021844422U;
+        static const AkUniqueID VO_ISAAC_VOICELINE6 = 2021844421U;
+        static const AkUniqueID VO_ISAAC_VOICELINE7 = 2021844420U;
+        static const AkUniqueID VO_RESCUESHIP_NARRATIVEEND = 3906700528U;
+        static const AkUniqueID VO_RESCUESHIP_RESPONSE = 2704646508U;
+        static const AkUniqueID VO_SERGEANT_FIFHTCOMPUTER1 = 3072456904U;
+        static const AkUniqueID VO_SERGEANT_GENERAL1 = 1212771736U;
+        static const AkUniqueID VO_SERGEANT_GENERAL2 = 1212771739U;
+        static const AkUniqueID VO_SERGEANT_GENERAL3 = 1212771738U;
+        static const AkUniqueID VO_SERGEANT_GENERAL4 = 1212771741U;
+        static const AkUniqueID VO_SERGEANT_WAKEMED1 = 1695989290U;
+        static const AkUniqueID VO_SERGEANT_WAKEMED2 = 1695989289U;
+        static const AkUniqueID VO_SERGEANT_WAKEMED3 = 1695989288U;
+        static const AkUniqueID VO_SERGEANT_WAKEMED4 = 1695989295U;
+        static const AkUniqueID VO_SERGEANT_WAKEMED5 = 1695989294U;
+        static const AkUniqueID VO_SERGEANT_WAKEMED6 = 1695989293U;
+        static const AkUniqueID VO_STUB_BACKCAMP1 = 2462372217U;
+        static const AkUniqueID VO_STUB_CLEARROOM1 = 1857914381U;
+        static const AkUniqueID VO_STUB_CLEARROOM2 = 1857914382U;
+        static const AkUniqueID VO_STUB_CLEARROOM3 = 1857914383U;
+        static const AkUniqueID VO_STUB_CRASH1 = 2621040692U;
+        static const AkUniqueID VO_STUB_CRASH2 = 2621040695U;
+        static const AkUniqueID VO_STUB_DOOR1 = 26695775U;
+        static const AkUniqueID VO_STUB_DOOR2 = 26695772U;
+        static const AkUniqueID VO_STUB_ENDMISSIONNOAH1 = 4270907780U;
+        static const AkUniqueID VO_STUB_ENDMISSIONNOAH2 = 4270907783U;
+        static const AkUniqueID VO_STUB_FIFTHCOMPUTER1 = 485738767U;
+        static const AkUniqueID VO_STUB_FIGHTING1 = 827082109U;
+        static const AkUniqueID VO_STUB_FIGHTING2 = 827082110U;
+        static const AkUniqueID VO_STUB_FIGHTING3 = 827082111U;
+        static const AkUniqueID VO_STUB_FIGHTING4 = 827082104U;
+        static const AkUniqueID VO_STUB_FINDING1 = 3328660762U;
+        static const AkUniqueID VO_STUB_FINDING2 = 3328660761U;
+        static const AkUniqueID VO_STUB_FINDING3 = 3328660760U;
+        static const AkUniqueID VO_STUB_FIRSTALIEN = 3069220365U;
+        static const AkUniqueID VO_STUB_FIRSTAMIRA1 = 1225130969U;
+        static const AkUniqueID VO_STUB_FIRSTAMIRA2 = 1225130970U;
+        static const AkUniqueID VO_STUB_FIRSTAMIRA3 = 1225130971U;
+        static const AkUniqueID VO_STUB_FIRSTCOMPUTER1 = 2306385480U;
+        static const AkUniqueID VO_STUB_FIRSTNOAH1 = 207909039U;
+        static const AkUniqueID VO_STUB_FIRSTNOAH2 = 207909036U;
+        static const AkUniqueID VO_STUB_FIRSTNOAH3 = 207909037U;
+        static const AkUniqueID VO_STUB_FOURTHCOMPUTER1 = 633519362U;
+        static const AkUniqueID VO_STUB_FOURTHCOMPUTER2 = 633519361U;
+        static const AkUniqueID VO_STUB_GUARDWEAPON = 2456146089U;
+        static const AkUniqueID VO_STUB_HEAL1 = 2343389349U;
+        static const AkUniqueID VO_STUB_HEAL2 = 2343389350U;
+        static const AkUniqueID VO_STUB_HEAL3 = 2343389351U;
+        static const AkUniqueID VO_STUB_HURT1 = 1393191702U;
+        static const AkUniqueID VO_STUB_MEDCLEAR1 = 835901656U;
+        static const AkUniqueID VO_STUB_MEDCLEAR2 = 835901659U;
+        static const AkUniqueID VO_STUB_MISSIONAMIRA1 = 1255446693U;
+        static const AkUniqueID VO_STUB_MISSIONAMIRA2 = 1255446694U;
+        static const AkUniqueID VO_STUB_MISSIONAMIRA3 = 1255446695U;
+        static const AkUniqueID VO_STUB_MISSIONAMIRA4 = 1255446688U;
+        static const AkUniqueID VO_STUB_OUTSHIP = 1792990868U;
+        static const AkUniqueID VO_STUB_OUTSIDENOAH1 = 2130010906U;
+        static const AkUniqueID VO_STUB_PLANETINFO1 = 2925042445U;
+        static const AkUniqueID VO_STUB_PLANETINFO2 = 2925042446U;
+        static const AkUniqueID VO_STUB_PLANETINFO3 = 2925042447U;
+        static const AkUniqueID VO_STUB_SECONDCOMPUTER1 = 3696739954U;
+        static const AkUniqueID VO_STUB_SECONDCOMPUTER2 = 3696739953U;
+        static const AkUniqueID VO_STUB_SECONDCOMPUTER3 = 3696739952U;
+        static const AkUniqueID VO_STUB_THIRDCOMPUTER1 = 2301289131U;
+        static const AkUniqueID VO_STUB_WAKEMED1 = 3513509957U;
+        static const AkUniqueID VO_STUB_WAKEMED2 = 3513509958U;
+        static const AkUniqueID VO_STUB_WAKEMED3 = 3513509959U;
+        static const AkUniqueID VO_STUB_WAKEMED4 = 3513509952U;
+        static const AkUniqueID VO_STUB_WAKEMED5 = 3513509953U;
+        static const AkUniqueID VO_STUB_XENOFIGHT1 = 3871246353U;
+        static const AkUniqueID VO_STUB_XENOFIGHT2 = 3871246354U;
+        static const AkUniqueID VO_STUB_XENOFIGHT3 = 3871246355U;
+        static const AkUniqueID VO_STUB_XENOFIGHTEND1 = 3353530420U;
+        static const AkUniqueID W_M4_RELOAD = 2147632472U;
+        static const AkUniqueID W_M4_SHOOT = 933371860U;
+        static const AkUniqueID W_SL_SHOOT = 3010972734U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace GAMEPLAYMODE
+        {
+            static const AkUniqueID GROUP = 15699892U;
+
+            namespace STATE
+            {
+                static const AkUniqueID CAMP = 3736508534U;
+                static const AkUniqueID COMBAT = 2764240573U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID RUINS = 417916826U;
+                static const AkUniqueID SHIP = 284967655U;
+            } // namespace STATE
+        } // namespace GAMEPLAYMODE
+
+    } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace SURFACETYPE
+        {
+            static const AkUniqueID GROUP = 63790334U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID DIRT = 2195636714U;
+                static const AkUniqueID SHIP = 284967655U;
+            } // namespace SWITCH
+        } // namespace SURFACETYPE
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID SS_AIR_FEAR = 1351367891U;
+        static const AkUniqueID SS_AIR_FREEFALL = 3002758120U;
+        static const AkUniqueID SS_AIR_FURY = 1029930033U;
+        static const AkUniqueID SS_AIR_MONTH = 2648548617U;
+        static const AkUniqueID SS_AIR_PRESENCE = 3847924954U;
+        static const AkUniqueID SS_AIR_RPM = 822163944U;
+        static const AkUniqueID SS_AIR_SIZE = 3074696722U;
+        static const AkUniqueID SS_AIR_STORM = 3715662592U;
+        static const AkUniqueID SS_AIR_TIMEOFDAY = 3203397129U;
+        static const AkUniqueID SS_AIR_TURBULENCE = 4160247818U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID THEONEENGINE = 3491347560U;
+        static const AkUniqueID VOICEOVER = 4041657371U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID SFX = 393239870U;
+        static const AkUniqueID VOICES = 3313685232U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
