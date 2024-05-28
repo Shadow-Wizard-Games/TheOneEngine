681372e5aefd063ab3bb2a1607155a3c7eb76588
Historn <arnaujiga@gmail.com>
2024-05-28 23:41:43 +0200

    Added Level1 shortcuts + fixed error going back to L1R1
    Need to fix Enable and Disable of L1R1 Shortcut

--- TheOneScripting/GameManager.cs
+++ TheOneScripting/GameManager.cs
@@ -18,6 +18,9 @@ public class GameManager : MonoBehaviour
 
     public List<string> savedLevels = new List<string>();
 
+    //Historn: Shortcuts hardcoded
+    public bool activeShortcutL1R1 = false;
+
     public override void Start()
     {
         hasSaved = false;
@@ -36,7 +39,18 @@ public class GameManager : MonoBehaviour
 
     public override void Update()
     {
-
+        if (SceneManager.GetCurrentSceneName() == "L1R1" && savedLevels.Contains("L1R4") && !activeShortcutL1R1)
+        {
+            //shortcutToL1R4 = IGameObject.Find("S");
+            Debug.Log("Shortcut L1R1 To L1R4 available");
+            IGameObject.Find("SwapScene_L1_R4").Enable();
+            activeShortcutL1R1 = true;
+        }
+        if (SceneManager.GetCurrentSceneName() == "L1R1" && !savedLevels.Contains("L1R4"))
+        {
+            IGameObject.Find("SwapScene_L1_R4").Disable();
+        }
+        //Debug.Log("Num of scenes saved " + savedLevels.Count); 
     }
 
     public void SaveSceneState()
