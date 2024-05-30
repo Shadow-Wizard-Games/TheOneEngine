f2a64b17f8491a5ec1a05544f8c51c0067771ad8
JanNetoLlorens <99729455+JanNetoLlorens@users.noreply.github.com>
2024-05-30 12:53:28 +0200

    Added item shoulder laser

---
+++ TheOneScripting/Item_ShoulderLaser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Item_ShoulderLaser : Item
+{
+    public readonly uint damage = 15;
+    public readonly float fireRate = 0.4f;
+
+    public Item_ShoulderLaser()
+    {
+        this.id = 6;
+        this.name = "ShoulderLaser";
+        this.description = "Shoulder attachement which shoots automatically and pierces through enemies";
+        this.type = ItemType.WEAPON;
+    }
+
+    public override void ApplyEffect(IGameObject player)
+    {
+
+    }
+}
