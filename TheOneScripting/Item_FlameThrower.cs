81539ed35a53f6f762951174bfbe4b3a70b7dc87
Adrián Ponce <adrianponcexxv@gmail.com>
2024-06-09 23:22:23 +0200

    enemies changed

--- TheOneScripting/Item_FlameThrower.cs
+++ TheOneScripting/Item_FlameThrower.cs
@@ -2,13 +2,13 @@
 
 public class Item_FlameThrower : Item
 {
-    public readonly int damage = 9;
+    public readonly int damage = 4;
     public readonly float slowAmountEnemy = 0.3f;
 
     public readonly float activeTime = 10f;
     public readonly float cooldownTime = 20f;
 
-    public readonly float intervalTime = 0.12f;
+    public readonly float intervalTime = 0.3f;
 
     public Item_FlameThrower()
     {
